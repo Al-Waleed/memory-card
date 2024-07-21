602f9c9ca9539692373d977471d06810bf1a19f4
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-07-21 16:42:33 +0300

    Create a card component that takes the cards array, shuffles it and creates an img element for the first 12 cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,22 @@
-export default function Card() {
-  return (
-    <>
-      
-    </>
-)
+export default function Card({ cards }) {
+  // to shuffle the cards
+  const shuffledCards = shuffle(cards)
+
+  // to take the first 12 cards
+  const firstTwelve = shuffledCards.filter((card, index) => index < 12)
+
+// 
+  const cardsList = firstTwelve.map((card) => {
+    return <img src={card} key={card} />;
+  });
+  return <div id="cards">{cardsList}</div>;
+}
+
+// A function that shuffles an array randomly
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array
 }
